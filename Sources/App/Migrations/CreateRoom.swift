ac0965a4480c18743cf81bf0f5326f63a2867995
alisa <alisavy2010@gmail.com>
2023-04-02 14:18:10 +0300

    commit

--- Sources/App/Migrations/CreateRoom.swift
+++ Sources/App/Migrations/CreateRoom.swift
@@ -9,9 +9,12 @@ import Fluent
 
 struct CreateRoom: AsyncMigration {
     
-    func randInvintationCode() -> String {
-      let letters = "0123456789"
-      return String((0..<10).map{ _ in letters.randomElement()! })
+    func randInvintationCode(isPrivate: Bool) -> String? {
+        if !isPrivate {
+            let letters = "0123456789"
+            return String((0..<10).map{ _ in letters.randomElement()! })
+        }
+        return nil
     }
     
     func prepare(on database: Database) async throws {
@@ -21,7 +24,7 @@ struct CreateRoom: AsyncMigration {
             .field("adminId", .uuid, .required)
             .field("permissin", .bool, .required)
             .field("scorePerWord", .int8, .required)
-            .field("invintationCode", .string, .custom(randInvintationCode()))
+            .field("invintationCode", .string, .custom(randInvintationCode(isPrivate: false)))
             .field("isGameStarted", .bool, .custom(false))
             .field("scoreToWin", .int8, .required)
             .field("roundTime", .double, .required)
